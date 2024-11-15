0af65a419ffef8ad9c0072ee359687306b079e08
hendisantika <hendisantika@yahoo.co.id>
2024-11-15 11:08:06 +0700

    Add taskScheduler in SchedulerConfiguration🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/scheduler/config/SchedulerConfiguration.java
+++ src/main/java/id/my/hendisantika/scheduler/config/SchedulerConfiguration.java
@@ -2,9 +2,13 @@ package id.my.hendisantika.scheduler.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,4 +33,30 @@ public class SchedulerConfiguration implements SchedulingConfigurer {
 
     @Value("${spring.scheduler.cron}")
     private String cronExpression;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setTaskScheduler(taskScheduler());
+//        taskRegistrar.addTriggerTask(
+//                () -> System.out.println("Scheduling task at "+ System.currentTimeMillis()),
+//                triggerContext -> {
+//                    Optional<Instant> lastCompletionTime = Optional.ofNullable(triggerContext.lastCompletion());
+//                    Instant nextExecutionTime = lastCompletionTime.orElseGet(Instant::now).plusMillis(delay * 1000);
+//                    return nextExecutionTime;
+//                });
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(poolSize);
+        scheduler.setThreadNamePrefix("ThreadScheduler-");
+        scheduler.initialize();
+        return scheduler;
+    }
+
+//    @Bean
+//    public CronTrigger cronTrigger(){
+//        return new CronTrigger(cronExpression);
+//    }
 }
