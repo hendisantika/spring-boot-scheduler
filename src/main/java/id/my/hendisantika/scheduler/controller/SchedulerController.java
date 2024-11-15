aaf7070df1687f3187156e7ebdc25613ef406a2e
hendisantika <hendisantika@yahoo.co.id>
2024-11-15 11:10:49 +0700

    Add sayHelloWithName in SchedulerController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/scheduler/controller/SchedulerController.java
+++ src/main/java/id/my/hendisantika/scheduler/controller/SchedulerController.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.scheduler.controller;
 
+import id.my.hendisantika.scheduler.entity.Job;
 import id.my.hendisantika.scheduler.service.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -29,4 +32,11 @@ public class SchedulerController {
     public String sayHello() {
         return "Hello World! " + LocalDateTime.now();
     }
+
+
+    @PostMapping("/hello")
+    public String sayHelloWithName(@RequestBody Job job) {
+        return "Hello " + job.getJobName();
+    }
+
 }
