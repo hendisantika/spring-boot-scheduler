1d0699b9d6429872efac2b8e33abafe5976eb438
hendisantika <hendisantika@yahoo.co.id>
2024-11-15 11:06:48 +0700

    Add scheduleGetJob in SchedulerService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/scheduler/service/SchedulerService.java
+++ src/main/java/id/my/hendisantika/scheduler/service/SchedulerService.java
@@ -4,12 +4,15 @@ import id.my.hendisantika.scheduler.entity.Job;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,5 +41,26 @@ public class SchedulerService {
             schedulerMap.remove(jobId);
         else
             log.error("No jobs exists with Id:{}", jobId);
+    }
+
+    public void scheduleGetJob(Job job) {
+        log.info("scheduling get job:{}", job.toString());
+        taskScheduler.schedule(
+                () -> {
+                    ResponseEntity<String> response = webClient.get()
+                            .uri(job.getBaseURL() + job.getApiURL())
+                            .header("Content-Type", "application/json")
+                            .retrieve()
+                            .toEntity(String.class)
+                            .block();
+                    if (response.getStatusCode().isError()) {
+                        log.error("Failure calling Job:{}with URL:{}{}", job.getJobName(), job.getBaseURL(), job.getApiURL());
+                    } else {
+                        log.info(response.getBody());
+                    }
+                },
+                new CronTrigger(job.getCron(), TimeZone.getTimeZone(TimeZone.getDefault().toZoneId()))
+        );
+        schedulerMap.put(job.getJobName() + "-" + System.currentTimeMillis(), job);
     }
 }
